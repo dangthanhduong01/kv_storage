11dec83e63f2813bbf63c5428d8bcea0182200b7
j4me5 <duongdt@sonek.vn>
2026-05-13 23:59:26 +0700

    feat: update document

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-A Key-value Storage using Thrift
+# KV Storage
+
+Key-value storage written in Go. The project uses Apache Thrift for RPC layer, PostgreSQL as persistent storage
 
 
-## How to run
+## Require
+
+- Go `1.24.5`
+- PostgreSQL 16
+- Apache Thrift compiler.
+
+## Environment
+
+```bash
+export DNS="user=postgres dbname=storage sslmode=disable password=1234 host=localhost port=5432"
+export HOST="localhost" # host server
+export PORT="9090" # port server
+```
+
+## Database
+
+Start PostgreSQL local with Docker:
+
+```bash
+./startdb.sh
+```
+
+Create data:
+
+```bash
+docker exec dbos-db psql -U postgres -c "CREATE DATABASE storage;"
+docker exec dbos-db psql -U postgres -d storage -c "CREATE TABLE wallet (key varchar(255) PRIMARY KEY, value TEXT);"
+```
+
+## Run Thrift server
+
+`go run ./server`
+
+## Run HTTP API gateway
+
+`go run ./client`
+
+API client listen to `localhost:8080`. Swagger UI:
+
+```text
+http://localhost:8080/docs/index.html
+```
+
+## Generate Thrift code
+
+`./gen_thrift.sh`
+
+Or
+
+```bash
+thrift -r --gen go ./kv.thrift
+```
+
+## Run test in all project
+
+```bash
+go test ./...
+```
+
+### Run test in dir `test`:
+
+```bash
+go test ./test
+```
--- client/transport.go
+++ client/transport.go
@@ -1,1 +1,66 @@
 package main
+
+import (
+	"kvstorage/gen-go/kv"
+	"kvstorage/thriftpool"
+	"time"
+
+	"github.com/apache/thrift/lib/go/thrift"
+)
+
+func dial(addr, port string, connTimeout time.Duration) (*thriftpool.IdleClient, error) {
+	socket, err := thrift.NewTSocketTimeout(addr+":"+port, connTimeout, connTimeout)
+	if err != nil {
+		return nil, err
+	}
+	transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTBufferedTransportFactory(8192))
+	protocolFactory := thrift.NewTCompactProtocolFactory()
+	tf, err := transportFactory.GetTransport(socket)
+	if err != nil {
+		return nil, err
+	}
+
+	client := kv.NewStorageServiceClientFactory(tf, protocolFactory)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tf.Open()
+	if err != nil {
+		return nil, err
+	}
+	return &thriftpool.IdleClient{
+		Client: client,
+		Socket: socket,
+		Host:   addr,
+		Port:   port,
+	}, nil
+}
+
+func close(c *thriftpool.IdleClient) error {
+	err := c.Socket.Close()
+	return err
+}
+
+var bsGenericMapPool = thriftpool.NewMapPool(1000, 5, 3600, dial, close)
+
+func GetKVClient(addr, port string) *thriftpool.IdleClient {
+	client, err := bsGenericMapPool.Get(addr, port).Get()
+	if err != nil {
+		return nil
+	}
+	return client
+}
+
+func BackToPool(c *thriftpool.IdleClient) {
+	if c == nil {
+		return
+	}
+	bsGenericMapPool.Get(c.Host, c.Port).Put(c)
+}
+func ServiceDisconnect(c *thriftpool.IdleClient) {
+	if c == nil {
+		return
+	}
+	bsGenericMapPool.Release(c.Host, c.Port)
+}
